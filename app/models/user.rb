bee3bb35e578a9f507cce9d93f3d4c9502947d27
Shalini Roy <1705820@kiit.ac.in>
2024-05-16 21:59:55 -0700

    Add User

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :journal_entries
+  has_many :anxiety_levels
+end
